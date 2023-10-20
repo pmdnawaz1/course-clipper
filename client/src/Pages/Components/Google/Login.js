f338133f6444437ae371ad326d06cf3862d6803b
sarfarazP <sarfaraz@loyalytics.in>
2023-10-20 23:31:06 +0530

    change backend host

--- client/src/Pages/Components/Google/Login.js
+++ client/src/Pages/Components/Google/Login.js
@@ -1,40 +1,47 @@
-import React , {useContext} from 'react'
-import {GoogleLogin} from 'react-google-login';
+import React, { useContext } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../context/authContext';
 import axios from 'axios';
 
-const ClientId = '458159501348-1cvbb6hb8hte66at0fqv831lascvm4m1.apps.googleusercontent.com';  
+const ClientId =
+	'458159501348-1cvbb6hb8hte66at0fqv831lascvm4m1.apps.googleusercontent.com';
 function Login() {
-     const navigate = useNavigate();
-    const {dispatch} = useContext(AuthContext);
-  const onSuccess = (res)=>{
-    console.log(res.profileObj);
-    const response = axios.post("http://localhost:3001/google/signin" , res.profileObj).then((res)=>{
-      if(res.data){
-        console.log('login_details',res.data);
-        // alert("YOUR GMAIL LOGIN PASSWORD IS YOUR EMAIL");
-        dispatch({type : 'LOGIN' , payload : res.data});
-        localStorage.setItem("User",JSON.stringify(res.data));
-        navigate("/review");
-      }
-    }).catch((err)=>{
-      console.log(err);
-    });
-    }
-    const onFailure = (res)=>{
-    console.log("LOGIN FAILED",res);
-    }
-  return (
-    <GoogleLogin
-    clientId={ClientId}
-    buttonText = "Signin with Google"
-    onSuccess={onSuccess}
-    onFailure={onFailure}
-    cookiePolicy={'single-host-origin'}
-    isSignedIn = {true}
-    />
-  )
+	const navigate = useNavigate();
+	const { dispatch } = useContext(AuthContext);
+	const onSuccess = (res) => {
+		console.log(res.profileObj);
+		const response = axios
+			.post(
+				'http://ec2-51-20-135-7.eu-north-1.compute.amazonaws.com/google/signin',
+				res.profileObj
+			)
+			.then((res) => {
+				if (res.data) {
+					console.log('login_details', res.data);
+					// alert("YOUR GMAIL LOGIN PASSWORD IS YOUR EMAIL");
+					dispatch({ type: 'LOGIN', payload: res.data });
+					localStorage.setItem('User', JSON.stringify(res.data));
+					navigate('/review');
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const onFailure = (res) => {
+		console.log('LOGIN FAILED', res);
+	};
+	return (
+		<GoogleLogin
+			clientId={ClientId}
+			buttonText="Signin with Google"
+			onSuccess={onSuccess}
+			onFailure={onFailure}
+			cookiePolicy={'single-host-origin'}
+			isSignedIn={true}
+		/>
+	);
 }
 
 export default Login;
